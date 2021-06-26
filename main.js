e061d49826857e13480ebd9a5907d8f7bf97e666
Aarav-mourya <78957289+Aarav-mourya@users.noreply.github.com>
2021-06-26 15:39:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+menu_list_array = ["Veg Margherita Pizza","Chicken Tandoori Pizza", "Cheses Burst Pizza", "Zingy Parcel","Non veg supream Pizza"
+                    ];
+
+function getmenu(){
+
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").Value;
+menu_list_array.sort();
+for(var i=0;i<menu_list_array.length;i++){
+    htmldata=htmldata+ '<li>' + menu_list_array[i] + '</li>'
+}
+htmldata=htmldata+"</ol>"
+document.getElementById("display_name").innerHTML = htmldata;
+}
+
+function add_item(){
+var htmldata;
+var item=document.getElementById("add_item").Value;
+menu_list_array
+
+
+}
+
+function add_top(){
+//Complete the code
+}
